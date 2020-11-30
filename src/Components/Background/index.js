7c328c3f19e00408bce1e6c683b0d36ce176eaae
Md Khalid Shahzad <khalidazam1997@gmail.com>
2020-11-30 16:37:28 +0530

    Added build file and firebase files and improved Code Styling

--- src/Components/Background/index.js
+++ src/Components/Background/index.js
@@ -2,147 +2,163 @@ import React from "react"
 import "./styles.css"
 import AppHeader from "../AppHeader/index.js"
 import SideBar from "../SideBar/index.js"
-import MapChart from "../MapChart/index.js";
+import MapChart from "../MapChart/index.js"
 
-class Background extends React.Component{
+class Background extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			isLoading: false,
+			loadSuccessful: false,
+			global: {},
+			countries: [],
+			currrentCountry: {},
+			coordinates: [],
+			countryCode: "",
+			latitude: 0,
+			longitude: 0,
+			zoom: 1.12,
+			plotCondition: 0,
+		}
+	}
 
-    constructor(props){
-        super(props);
-        this.state={
-            isLoading:false,
-            loadSuccessful: false,
-            global:{},
-            countries: [],
-            currrentCountry: {},
-            coordinates:[],
-            countryCode:"",
-            latitude:0,
-            longitude:0,
-            zoom:1.12,
-            plotCondition: 0
-        }
-    }
+	componentDidMount = () => {
+		this.fetchData()
+	}
 
-    componentDidMount = () => {
-        this.fetchData();
-    }
+	fetchData = () => {
+		const url = "https://api.covid19api.com/summary"
+		this.setState({ isLoading: true })
+		fetch(url, {
+			method: "GET",
+		})
+			.then((response) => {
+				return response.json()
+			})
+			.then((result) => {
+				this.fetchLatitudeLongitude(result)
+			})
+			.catch((error) => {
+				alert(
+					"Failed to retrieve data from server\nPlease check your Internet connection and try again"
+				)
+				this.setState({
+					isLoading: false,
+					loadSuccessful: false,
+				})
+			})
+	}
 
-    fetchData = () => {
-        const url="https://api.covid19api.com/summary"
-        this.setState({isLoading:true})
-        fetch(url,{
-            method: "GET"
-        }).then((response) => {
-            return response.json()
-        }).then((result) => {
-            this.fetchLatitudeLongitude(result)
-        }).catch((error) => {
-            alert("Failed to retrieve data from server\nPlease check your Internet connection and try again");
-            this.setState({
-                isLoading: false,
-                loadSuccessful: false
-            })
-        })
-    }
-    
-    fetchLatitudeLongitude = (result) => {
-        let countryData={},newCoordinates=[]
-        const url_latLon="https://www.trackcorona.live/api/countries"
-        fetch(url_latLon,{
-            method:"GET"
-        }).then((response) => {
-            return response.json()
-        }).then((res) => {            
-            newCoordinates=res.data.map((country) => {
-                return {
-                    CountryCode: country.country_code.toUpperCase(),
-                    Latitude: country.latitude,
-                    Longitude: country.longitude
-                }
-            })
-            countryData=result.Countries.find((s)=>s.Country==="United States of America")
-            this.setState({
-                isLoading:false,
-                loadSuccessful: true,
-                global:result.Global,
-                countries:result.Countries,
-                currrentCountry:countryData,
-                coordinates:newCoordinates
-            })
-        })        
-        .catch((error) => {
-            alert("Failed to retrieve data from server\nPlease check your Internet connection and try again");
-            this.setState({
-                isLoading: false,
-                loadSuccessful: false
-            })
-        })
-    }
+	fetchLatitudeLongitude = (result) => {
+		let countryData = {},
+			newCoordinates = []
+		const url_latLon = "https://www.trackcorona.live/api/countries"
+		fetch(url_latLon, {
+			method: "GET",
+		})
+			.then((response) => {
+				return response.json()
+			})
+			.then((res) => {
+				newCoordinates = res.data.map((country) => {
+					return {
+						CountryCode: country.country_code.toUpperCase(),
+						Latitude: country.latitude,
+						Longitude: country.longitude,
+					}
+				})
+				countryData = result.Countries.find(
+					(s) => s.Country === "United States of America"
+				)
+				this.setState({
+					isLoading: false,
+					loadSuccessful: true,
+					global: result.Global,
+					countries: result.Countries,
+					currrentCountry: countryData,
+					coordinates: newCoordinates,
+				})
+			})
+			.catch((error) => {
+				alert(
+					"Failed to retrieve data from server\nPlease check your Internet connection and try again"
+				)
+				this.setState({
+					isLoading: false,
+					loadSuccessful: false,
+				})
+			})
+	}
 
-    changeCoordinates= (c) => { 
-        const d=this.state.coordinates.find((s) => s.CountryCode===c.CountryCode)
-        if(d){
-            this.setState({
-                countryCode: d.CountryCode,
-                latitude: d.Latitude,
-                longitude: d.Longitude,
-                zoom:3
-            })
-        }else{
-            this.setState({
-                countryCode: "",
-                latitude: 0,
-                longitude: 0,
-                zoom: 1.12
-            })
-        }
-    }
+	changeCoordinates = (c) => {
+		const d = this.state.coordinates.find(
+			(s) => s.CountryCode === c.CountryCode
+		)
+		if (d) {
+			this.setState({
+				countryCode: d.CountryCode,
+				latitude: d.Latitude,
+				longitude: d.Longitude,
+				zoom: 3,
+			})
+		} else {
+			this.setState({
+				countryCode: "",
+				latitude: 0,
+				longitude: 0,
+				zoom: 1.12,
+			})
+		}
+	}
 
-    plotByDeathCases = (e) => {
-        if(this.state.plotCondition !== 1){
-            this.setState({
-                plotCondition: 1
-            })
-        }
-    }
-    plotByConfirmedCases = (e) => {
-        if(this.state.plotCondition !== 0){
-            this.setState({
-                plotCondition: 0
-            })
-        }
-    }
+	plotByDeathCases = (e) => {
+		if (this.state.plotCondition !== 1) {
+			this.setState({
+				plotCondition: 1,
+			})
+		}
+	}
+	plotByConfirmedCases = (e) => {
+		if (this.state.plotCondition !== 0) {
+			this.setState({
+				plotCondition: 0,
+			})
+		}
+	}
 
-    render = () => {        
-        return(            
-            <div className="container-fluid app-background">
-                <AppHeader 
-                    isLoading={this.state.isLoading} 
-                    loadSuccessful={this.state.loadSuccessful} 
-                    data={this.state.global}
-                />
-                <div className="row app-body-cont">
-                    <div className="col-md-3 country-cont">
-                        <SideBar isLoading={this.state.isLoading} 
-                            loadSuccessful={this.state.loadSuccessful}
-                            countries={this.state.countries}
-                            currrentCountry={this.state.currrentCountry}
-                            changeCoordinates={this.changeCoordinates}
-                        />
-                    </div>
-                    <div className="col-md-9 map-cont"><MapChart countries={this.state.countries}
-                                                        countryCode={this.state.countryCode}
-                                                        latitude={this.state.latitude}
-                                                        longitude={this.state.longitude}
-                                                        zoom={this.state.zoom}
-                                                        plotByConfirmedCases={this.plotByConfirmedCases}
-                                                        plotByDeathCases={this.plotByDeathCases}
-                                                        plotCondition={this.state.plotCondition}
-                                                        />
-                    </div>
-                </div>
-            </div>
-        )
-    }
+	render = () => {
+		return (
+			<div className="container-fluid app-background">
+				<AppHeader
+					isLoading={this.state.isLoading}
+					loadSuccessful={this.state.loadSuccessful}
+					data={this.state.global}
+				/>
+				<div className="row app-body-cont">
+					<div className="col-md-3 country-cont">
+						<SideBar
+							isLoading={this.state.isLoading}
+							loadSuccessful={this.state.loadSuccessful}
+							countries={this.state.countries}
+							currrentCountry={this.state.currrentCountry}
+							changeCoordinates={this.changeCoordinates}
+						/>
+					</div>
+					<div className="col-md-9 map-cont">
+						<MapChart
+							countries={this.state.countries}
+							countryCode={this.state.countryCode}
+							latitude={this.state.latitude}
+							longitude={this.state.longitude}
+							zoom={this.state.zoom}
+							plotByConfirmedCases={this.plotByConfirmedCases}
+							plotByDeathCases={this.plotByDeathCases}
+							plotCondition={this.state.plotCondition}
+						/>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
-export default Background;
+export default Background
--- src/Components/MapChart/index.js
+++ src/Components/MapChart/index.js
@@ -1,91 +1,122 @@
-import React from "react";
-import { scaleLinear } from "d3-scale";
+import React from "react"
+import { scaleLinear } from "d3-scale"
 import {
-    ComposableMap,
-    Geographies,
-    Geography,
-    ZoomableGroup,
-    Sphere,
-    Graticule
-  } from "react-simple-maps";
+	ComposableMap,
+	Geographies,
+	Geography,
+	ZoomableGroup,
+	Sphere,
+	Graticule,
+} from "react-simple-maps"
 import "./styles.css"
 
-  const MapChart = (props) => {
-    const geoUrl =
-    "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+const MapChart = (props) => {
+	const geoUrl =
+		"https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json"
 
-    const deathPMScale = scaleLinear()
-    .domain([0, 1500])
-    .range(["#FFE1C3", "#CE0A05"]);
+	const deathPMScale = scaleLinear()
+		.domain([0, 1500])
+		.range(["#FFE1C3", "#CE0A05"])
 
-    const confirmedPMScale = scaleLinear()
-    .domain([0, 55000])
-    .range(["#FFE1C3", "#CE0A05"]);
+	const confirmedPMScale = scaleLinear()
+		.domain([0, 55000])
+		.range(["#FFE1C3", "#CE0A05"])
 
-    const totalCasesPerMillion = (totalCases,population) => {
-      const result=totalCases*1000000/population
-      return(confirmedPMScale(result))
-    }
+	const totalCasesPerMillion = (totalCases, population) => {
+		const result = (totalCases * 1000000) / population
+		return confirmedPMScale(result)
+	}
 
-    const totalDeathsPerMillion = (totalDeaths,population) => {
-      const r=totalDeaths*1000000/population
-      return(deathPMScale(r))
-    }
+	const totalDeathsPerMillion = (totalDeaths, population) => {
+		const r = (totalDeaths * 1000000) / population
+		return deathPMScale(r)
+	}
 
-    return (
-        <div className="map-container">
-          <div className="button-container">
-            <button 
-              className={props.plotCondition?"button":"button button-active"}
-              onClick={props.plotByConfirmedCases}>
-                Total Cases (per million)
-            </button>
-            <button 
-              className={props.plotCondition?"button button-active":"button"}
-              onClick={props.plotByDeathCases}>
-                Total Deaths (per million)
-            </button>
-          </div>
+	return (
+		<div className="map-container">
+			<div className="button-container">
+				<button
+					className={props.plotCondition ? "button" : "button button-active"}
+					onClick={props.plotByConfirmedCases}
+				>
+					Total Cases (per million)
+				</button>
+				<button
+					className={props.plotCondition ? "button button-active" : "button"}
+					onClick={props.plotByDeathCases}
+				>
+					Total Deaths (per million)
+				</button>
+			</div>
 
-          <div className="color-palette-container">
-          <div className="case-indicator">
-              <span>Low</span>
-              <span>High</span>
-            </div>
-            <div className="color-palette">
-              <span className="span-1"></span><span className="span-2"></span><span className="span-3"></span>
-              <span className="span-4"></span><span className="span-5"></span><span className="span-6"></span>
-            </div>            
-          </div>
+			<div className="color-palette-container">
+				<div className="case-indicator">
+					<span>Low</span>
+					<span>High</span>
+				</div>
+				<div className="color-palette">
+					<span className="span-1"></span>
+					<span className="span-2"></span>
+					<span className="span-3"></span>
+					<span className="span-4"></span>
+					<span className="span-5"></span>
+					<span className="span-6"></span>
+				</div>
+			</div>
 
-          <ComposableMap width={1050}>
-            <ZoomableGroup center={[props.longitude,props.latitude]} zoom={props.zoom}>
-            <Sphere stroke="#E4E5E6" strokeWidth={0.5}/>
-            <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-              <Geographies geography={geoUrl}>
-                {({ geographies }) =>
-                  geographies.map((geo) => {
-                    const d = props.countries.find((s) => s.CountryCode === geo.properties.ISO_A2);
-                    const c = geo.properties.ISO_A2 === props.countryCode
-                    return(
-                        <Geography key={geo.rsmKey} geography={geo} stroke="#000000" strokeWidth={c ? 1.25 : 0.1 } 
-                        fill={d?(
-                            (props.plotCondition)?(totalDeathsPerMillion(d.TotalDeaths,geo.properties.POP_EST))
-                            :(totalCasesPerMillion(d.TotalConfirmed,geo.properties.POP_EST)))
-                          :"#FFFFFF"}/>
-                    )
-                  })
-                }
-              </Geographies>
-            </ZoomableGroup>
-          </ComposableMap>
-          
-          <p className="self">
-            Developed by <a  className="linkedin" href="https://www.linkedin.com/in/md-khalid-shahzad-7b63611b1" target="_blank" rel="noreferrer">
-              Md Khalid Shahzad</a>
-          </p>
-        </div>
-      );
-  }
+			<ComposableMap width={1050}>
+				<ZoomableGroup
+					center={[props.longitude, props.latitude]}
+					zoom={props.zoom}
+				>
+					<Sphere stroke="#E4E5E6" strokeWidth={0.5} />
+					<Graticule stroke="#E4E5E6" strokeWidth={0.5} />
+					<Geographies geography={geoUrl}>
+						{({ geographies }) =>
+							geographies.map((geo) => {
+								const d = props.countries.find(
+									(s) => s.CountryCode === geo.properties.ISO_A2
+								)
+								const c = geo.properties.ISO_A2 === props.countryCode
+								return (
+									<Geography
+										key={geo.rsmKey}
+										geography={geo}
+										stroke="#000000"
+										strokeWidth={c ? 1.25 : 0.1}
+										fill={
+											d
+												? props.plotCondition
+													? totalDeathsPerMillion(
+															d.TotalDeaths,
+															geo.properties.POP_EST
+													  )
+													: totalCasesPerMillion(
+															d.TotalConfirmed,
+															geo.properties.POP_EST
+													  )
+												: "#FFFFFF"
+										}
+									/>
+								)
+							})
+						}
+					</Geographies>
+				</ZoomableGroup>
+			</ComposableMap>
 
-  export default MapChart;
+			<p className="self">
+				Developed by{" "}
+				<a
+					className="linkedin"
+					href="https://www.linkedin.com/in/md-khalid-shahzad-7b63611b1"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Md Khalid Shahzad
+				</a>
+			</p>
+		</div>
+	)
+}
+export default MapChart
